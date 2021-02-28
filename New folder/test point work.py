35f27121f78554f18433f7085bcb19fc4794f714
Superbachelor <jiawei_lu@yeah.net>
2021-02-28 16:04:29 +0800

    Update test point work.py
    2021_02_28 update

--- New folder/test point work.py
+++ New folder/test point work.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Sat Feb 20 10:47:55 2021
 
@@ -25,49 +25,67 @@ def findmark(label, base):
         else:
             pass
     if x ==0:
-        print('not found')
+        print(label,' is not found')
         
     return x   
         
 
-def findresult(l, p, e, Q, bq, s3,file):
-    tex = pd.read_csv(file[0],encoding='ANSI',skiprows=3,sep=';')            
-#    tex.fillna(0,inplace=True)
+def findresult(l, p, e, Q, bq,partnu,s3,i):
+    seriesname = pd.read_csv(i, encoding='ISO-8859-1', nrows=1, sep=';', low_memory=False)
+
+    pnu = findmark(partnu, seriesname.columns)
+    injname = seriesname.loc[0, pnu]
+
+    tex = pd.read_csv(i,encoding='ANSI',skiprows=3,sep=';',low_memory=False)
+    tex.fillna(0, inplace=True)
     
-    x = findmark(l, tex.columns[0])
-    
-#    pr = findmark(p, tex.columns[0])
-#    et = findmark(e, tex.columns[0])
-#    inq = findmark(Q, tex.columns[0])
-#    baq = findmark(bq, tex.columns[0])
-#    ts = findmark(s3, tex.columns[0])
+    x = findmark(l, tex.columns)
+    pr = findmark(p, tex.columns)
+    et = findmark(e, tex.columns)
+    inq = findmark(Q, tex.columns)
+    baq = findmark(bq, tex.columns)
+    ts = findmark(s3, tex.columns)
+
+    # inq = findmark(Q, tex.columns[0])
+    # baq = findmark(bq, tex.columns[0])
+    # ts = findmark(s3, tex.columns[0])
+    # x = findmark(l, tex.columns[0])
     
                
-    k=tex.loc[tex[str(x)]=='Measure'].index[0]
-    print(k)
+    Pressure = tex.loc[tex[x]==' Measure'][pr]
+    Pressure.name = 'Pressure(bar)'
+    ET = tex.loc[tex[x] ==' Measure'][et]
+    ET.name = 'ET(us)'
+    table_head = pd.concat([Pressure,ET], axis=1).reset_index(drop=True)
+    # table_head2 = pd.concat([table_head,table_head,table_head],key=['injQ','BFQ','3S'], axis=1,ignore_index=True)
+    table_head2 = pd.concat([table_head, table_head, table_head], axis=0,keys=['injQ','BFQ','3S'], ignore_index=True)
+    print (table_head2)
 #    press=key[pr]
 #    print(press)
 #    press.name='Pressure'
 #    etime=key[et]
 #    etime.name='ET'
-            
-#   table_head = pd.concat([press,etime],axis=1).reset_index(drop=True)
+#     press = tex.loc[[pr], [key]]
+
+     # table_head = pd.concat([press,etime],axis=1).reset_index(drop=True)
     
-    #return key, pr, et, inq, baq, ts
-    return k
+    return x, pr, et, inq, baq, ts
+    # return key
 
 if __name__ == '__main__':
     file = openfile()
     
-    l = (' Lastpunkt','1')
+    l = [' Lastpunkt']
     p = [' P_Rail(S)_Soll']
     e = [' t_MI(S)_Soll']
-    Q = [' Q_emi2_MI(MV)']
-    bq = [' Q_Inj_BF(MV)']
-    s3 = [' Q_emi2_MI_3s(MV)']
+    Q = [' Q_emi2_MI(MV)', ' v1.mean(HDA_1)']
+    bq = [' Q_Inj_BF(MV)', ' Q_Backflow(M)']
+    s3 = [' Q_emi2_MI_3s(MV)',' V_Inj1StdAbw(HDA_1)']
+    partnu = [' Partnumber']
 
-#    key,pr,et,inq, baq, ts = findresult(l, p, e, Q, bq, s3, file)
-    key = findresult(l, p, e, Q, bq, s3, file)
+    key,pr,et,inq, baq, ts = findresult(l, p, e, Q, bq, partnu, s3, file[0])
+
+    # key = findresult(l, p, e, Q, bq, s3, file)
 
 
 
